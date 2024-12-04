738f486b44878a75dd28ac909a910c5c666ab08b
He-26 <gireeshpitchuka999@gmail.com>
2024-12-04 15:59:40 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/Task6JenkinsTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task6JenkinsTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task6JenkinsTestingApplication.class, args);
+	}
+
+}
